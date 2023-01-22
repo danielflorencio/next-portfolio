8908bcb801d60091276da2362b2fb04819ac4af7
danielflorencio <danielflorencio20@gmail.com>
2023-01-22 02:54:49 -0300

    improved the website

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
+import {BsFillArrowDownCircleFill} from 'react-icons/bs'
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Projects from '../src/components/Projects'
 import SkillBlocks from '../src/components/SkillBlocks'
+import { IconContext } from 'react-icons'
 
 const Home: NextPage = () => {
   return (
@@ -11,8 +14,20 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
+        <section className='bg-slate-100 flex justify-center pt-8'>
+          <nav className='bg-slate-200 w-4/6 h-24 flex border border-solid justify-between p-6 pr-14 rounded-3xl'>
+            <div className='ml-10 bg-black rounded-full w-14 h-14 text-white text-3xl flex justify-center items-center pb-2'>
+              {'</>'}
+            </div>
+            <ul className='flex'>
+              <li className='mr-8 text-lg'><strong><a>Skills</a></strong></li>
+              <li className='mr-8 text-lg'><strong><a>Projects</a></strong></li>
+              <li className='mr-8 text-lg'><strong><a>Resume</a></strong></li>
+            </ul>
+          </nav>
+        </section>
         <section className='flex w-full h-screen flex-1 flex-col items-center justify-center text-center bg-slate-100'>
-          <h1 className="text-6xl mt-2 font-bold">
+          <h1 className="text-6xl font-bold">
             Hi, I am{' '}
             <span className="text-blue-600">
               Daniel.
@@ -22,7 +37,14 @@ const Home: NextPage = () => {
           <p className="mt-12 text-4xl">
             And your search for a React developer has just ended.
           </p>
-          <p className='mt-28 text-1xl font-bold '>Scroll down to see my projects and the benefits you get by having me on your team.</p>
+          {/* <p className='mt-28 text-1xl font-bold '>Scroll down to see my projects and the benefits you get by having me on your team.</p> */}
+          <p className='mt-28 text-1xl text-gray-700 '>Scroll down to see my projects and the benefits you get with me on your team.</p>
+          
+          <div className='mt-10 float'>
+            <IconContext.Provider value={{size: '1.8rem'}}>
+              <BsFillArrowDownCircleFill/>
+            </IconContext.Provider>
+          </div>
         </section>
         <section className='w-full text-center'>
           <h2 className='font-bold text-4xl mt-12'>These are the technologies i specialize in.</h2>
@@ -31,9 +53,15 @@ const Home: NextPage = () => {
             <SkillBlocks/>
           </div>
         </section>
-        <section className='w-full'>
-          <h2 className='text-4x1 mt-12 font-bold'>These are some of the projects I've made.</h2>
-          
+        <section className='w-full text-center bg-slate-100'>
+          <h2 className=' text-4xl p-12 font-bold'>Take a look at some of the projects I've made.</h2>
+          <Projects/>
+        </section>
+        <section className='w-full text-center'>
+          <h2 className=' text-4xl mt-12 font-bold'>Title</h2>
+          <form>
+            <text></text>
+          </form>
         </section>
       </main>
     </div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,13 +1,60 @@
+import {FiExternalLink} from 'react-icons/fi'
+
 const projects = [
-    ['imgSrc', 'title', 'Project Description'],
-    []
-]
-
-
+    { imgSrc: 'fire-screenshot.png', 
+    title: 'Personal Finance Web App', 
+    descriptionParagraphs: 
+    ['This is Fire, a personal finance web application that helps people control their finances, take better financial decisions and retire as early as possible. This project has a handful of features all aimed towards one simple goal: making you get the best out of all of your money investments.', 
+    'The app fetches data from external APIS in order to better inform the user of their preferred stocks real-time data whenever they need it.', 
+    'It also helps them track all of their expenses, incomes, and will, in the future, use analytical data structures and algorithms to give insights on how to improve their spending, so they can accelerate their journey to financial independence.'], 
+    skills: ['HTML', 'CSS', 'React', 'Redux', 'Bootstrap', 'TypeScript'], 
+    link: 'https://danielflorencio.github.io/fire_advisor/',
+    direction: 'direct'},
+    
+    { imgSrc: 'aos-screenshot.png', 
+    title: 'AOS - The Application Optimizing System', 
+    descriptionParagraphs: 
+    ['A simple React application that helps software engineering professionals (and future professionals) track their efforts and prioritize their learning.', 
+    'It helps them optimize their progress by analyzing their technical job skills and technical interview skills, so they know exactly what to focus and thus avoid procrastination and feeling lost.',
+    'It also has a Word-frequency analyzing system, so they can analyze job postings descriptions and find the most common used keywords for their specific roles and beat the ATS (Applicant Tracking System).'], 
+    skills: ['HTML', 'CSS', 'React', 'TypeScript'], 
+    link: '',
+    direction: 'reverse'},
+    
+    { imgSrc: 'salus-screenshot.png', 
+    title: 'Digital Agency Website', 
+    descriptionParagraphs: 
+    ['Built a Website with the goal of acquiring new customers for a marketing agency that focuses on healthcare clinics and doctors as their main target audience.', 
+    'It is a fully responsive and conversion focused web page, where it’s unique goal is to generate meetings and finally sales.'], 
+    link: 'https://danielflorencio.github.io/rp-salusmidia/',
+    skills: ['HTML', 'CSS', 'React', 'JavaScript'], direction: 'direct'}
+] 
 
 export default function Projects(){
     return(
-        <>
-        </>
+        <div className="w-full mb-16 flex flex-col justify-center items-center">
+            {projects.map(project => (
+                <div className={project.direction === 'direct' ? 'my-12 w-4/5 h-fit flex flex-wrap' : 'flex-row-reverse my-12 w-4/5 h-fit flex flex-wrap'}>
+                    <div className=" h-full flex justify-center items-center w-1/2"><img className=" w-3/5 border border-solid rounded-lg shadow-2xl" src={project.imgSrc}></img></div>
+                    <div className="w-1/2 flex flex-col justify-between">
+                        <div>
+                            <div className='flex'>
+                                <h3 className="text-left py-3 text-2xl font-bold">{project.title}</h3><a href={project.link} className="hover:text-sky-400 transition-colors duration-150"><FiExternalLink/></a>
+                            </div>
+                            {project.descriptionParagraphs.map(paragraph =>(
+                                <p className="text-left">{paragraph}<br/><br/></p>
+                            ))}
+                        </div>
+                        <div className="flex p-4 flex-wrap">
+                            {project.skills.map(skill => (
+                                <div className="mr-4 border border-solid border-black w-fit h-8 p-2 flex justify-center items-center hover:text-white hover:bg-black transition-colors duration-150">{skill}</div>
+                            ))}
+                        </div>
+                    </div>
+                
+                
+                </div>
+            ))}
+        </div>
     )
 }
