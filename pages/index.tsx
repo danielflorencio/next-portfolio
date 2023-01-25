e77cc7f1f3aa8706bc5361b577655e2a5f9c992c
danielflorencio <danielflorencio20@gmail.com>
2023-01-25 12:40:13 -0300

    adjusted some spacings in the herosec

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,9 @@ import Head from 'next/head'
 import SkillBlocks from '../src/components/SkillBlocks'
 import { IconContext } from 'react-icons'
 import ContactSection from '../src/components/Contact'
-import { useMediaQuery } from 'react-responsive'
 import { FiExternalLink } from 'react-icons/fi'
 import Projects from '../src/components/Projects'
-
 const Home: NextPage = () => {
-  const isSmallScreen = useMediaQuery({ query: '(max-width: 768px)' }); 
   return (
     <div className='w-full'>
       <Head>
@@ -21,26 +18,26 @@ const Home: NextPage = () => {
       <main>
         <section className='bg-slate-100 flex justify-center pt-4 w-full'>
           <nav className='bg-slate-200 sm:w-11/12 lg:w-4/5 h-16 flex border border-solid border-slate-300 justify-between items-center rounded-3xl'>
-            <div className='bg-black p-2 ml-6 rounded-full text-white text-3xl flex justify-center items-center'>
+            <div className='bg-black p-2 ml-2 rounded-full text-white text-3xl flex justify-center items-center'>
               <AiOutlineCode/>
             </div>
             <ul className='flex text-slate-800 gap-3'>
-              <li className=' pl-4 lg:mr-8 text-lg font-semibold'><a className='hover:text-sky-400 transition-colors duration-150' href="#skills">Skills</a></li>
+              <li className=' pl-2 lg:mr-8 text-lg font-semibold'><a className='hover:text-sky-400 transition-colors duration-150' href="#skills">Skills</a></li>
               <li className=' lg:mr-8 text-lg font-semibold'><a className='hover:text-sky-400 transition-colors duration-150' href="#projects">Projects</a></li>
               <li className=' lg:mr-8 text-lg font-semibold'><a className='hover:text-sky-400 transition-colors duration-150' href="#contact" >Contact</a></li>
-              <li className=' lg:mr-8 text-lg font-semibold'><span className='flex pr-4 hover:text-sky-400 transition-colors duration-150'><a href="https://drive.google.com/file/d/1KB-gJxcTCpCg6ojEivJ0E1S7lW6X-0ep/view?usp=share_link">Resume</a><FiExternalLink/></span></li>
+              <li className=' lg:mr-8 text-lg font-semibold'><span className='flex pr-2 hover:text-sky-400 transition-colors duration-150'><a href="https://drive.google.com/file/d/1KB-gJxcTCpCg6ojEivJ0E1S7lW6X-0ep/view?usp=share_link">Resume</a><FiExternalLink/></span></li>
             </ul>
           </nav>
         </section>
         <section className='flex w-full h-screen flex-1 flex-col items-center justify-center text-center bg-slate-100'>
-          <h1 className="text-6xl font-bold">
+          <h1 className="sm:text-5xl sm:px-2 text-6xl font-bold">
             Hi, I'm{' '}
             <span className="text-blue-600">
               Daniel.
             </span>
           </h1>
 
-          <p className="mt-12 text-4xl">
+          <p className="mt-12 sm:text-3xl sm:px-2 text-4xl">
             And your search for a React developer has just ended.
           </p>
           <p className='mt-28 text-1xl text-gray-700 '>Scroll down to see my skills and projects.</p>
