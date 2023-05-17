9c76a290ac4ecd4a3ef30a7b986808140aaf981f
danielflorencio <danielflorencio20@gmail.com>
2023-05-17 08:04:37 -0300

    changed the .env.local in the next config to .env

--- next.config.js
+++ next.config.js
@@ -2,18 +2,9 @@
 const fs = require('fs');
 const dotenv = require('dotenv');
 dotenv.config();
-const env = dotenv.parse(fs.readFileSync('.env.local'));
+const env = dotenv.parse(fs.readFileSync('.env'));
 const nextConfig = {
   reactStrictMode: true,
   env: env
 }
-
 module.exports = nextConfig
-
-// /** @type {import('next').NextConfig} */
-// module.exports = {
-//   reactStrictMode: true,
-//   env: {
-//     NEXT_PUBLIC_GITHUB_KEY: '',
-//   },
-// }
