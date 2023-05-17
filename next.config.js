5c815f69021edee51e41fab024b06cd436527ce6
danielflorencio <danielflorencio20@gmail.com>
2023-05-17 08:08:01 -0300

    changed next config to add the new section.

--- next.config.js
+++ next.config.js
@@ -2,9 +2,12 @@
 const fs = require('fs');
 const dotenv = require('dotenv');
 dotenv.config();
-const env = dotenv.parse(fs.readFileSync('.env'));
+// const env = dotenv.parse(fs.readFileSync('.env'));
 const nextConfig = {
   reactStrictMode: true,
-  env: env
+  // env: env
+  env: {
+    customKey: 'value'
+  }
 }
 module.exports = nextConfig
